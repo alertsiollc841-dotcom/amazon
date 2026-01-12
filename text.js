807f8483339634f2bb13f2fc33a5458cda827811
alertsiollc841-dotcom <alertsiollc841@gmail.com>
2026-01-12 21:35:27 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,15 +1,15 @@
 window.defaultNumber = '+1-855-497-7034';
-window.defaultText = 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely! Immediately call Apple Support +1-855-497-7034 to freeze it';
+window.defaultText = 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely! Immediately call Amazon Support +1-855-497-7034 to freeze it';
 window.text = {
-    'xhamster.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'perfectgirls.net': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'gotporn.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'anysex.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'sex.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 unlock it!',
-    'bravotube.net': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'mylust.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'manporn.xxx': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'anybunny.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'txxx.com': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it',
-    'findbestsolution.xyz': 'Your |%model%| Amazon account was recently Charged at APPLE STORE For $349.99 Via Apple Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Apple Support +1-855-497-7034 to freeze it'
+    'xhamster.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'perfectgirls.net': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'gotporn.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'anysex.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'sex.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 unlock it!',
+    'bravotube.net': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'mylust.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'manporn.xxx': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'anybunny.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'txxx.com': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it',
+    'findbestsolution.xyz': 'Your  Amazon account was recently Charged at Amazon STORE For $349.99 Via Amazon Pay .if you want to stop this charge Completely . on |%ref%|! Immediately call Amazon Support +1-855-497-7034 to freeze it'
 };
